b477209b68f2590d1857b510dabd5870dcf7a5fa
Muyale Okoyo <annmuyale@gmail.com>
2023-03-09 09:02:01 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,59 @@
+fun main (){
+    //
+    work("Hello","world")
+    study("Complete")
+
+    var result = smart(arrayOf:)
+    var  XY  = "Hello"
+    var YZ= "world"
+    var write ="XY + YZ"
+    println(write)
+
+    add(20 ,10 ,30,40)
+
+}
+fun work(name1:String ,name2:String){
+    var result = "Hello world"
+    println(result[0])
+
+
+
+
+}
+fun study(name1: String){
+    var assign = "complete"
+    println(assign[0])
+    println(assign[1])
+    println(assign[2])
+    println(assign[3])
+    println(assign[4])
+    println(assign[5])
+    println(assign[6])
+    println(assign[7])
+
+}
+fun smart (arrayOf:(name1:String , name2:String ):Array<String>{
+    return(smart(arrayOf))
+
+}
+fun add (num1:Int , num2 :Int , num3:Int , num4: Int){
+    var sum = add(20 ,10)
+    var result = num1 +num2
+    println(result)
+    var subtract =add(20 ,10)
+    var answer= num1 - num2
+    println(answer)
+    println(subtract)
+    var division = add(20,10)
+    var give =num1/num2
+    var multiplication =add(20, 10)
+    var put=num1*num2
+    println(put)
+
+
+}
+
+
+
+
+
